c01316bc6802950dfb2756c4fe940f8c46c8e890
Noah <osuolale49@gmail.com>
2019-11-01 03:16:08 +0100

    updated

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@ docker push noalistic/ninostyle-client:$SHA
 docker push noalistic/ninostyle-server:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=noalistic/ninostyle-server:$SHA
 kubectl set image deployments/client-deployment client=noalistic/ninostyle-client:$SHA
+kubectl set image deployments/server-deployment server=noalistic/ninostyle-server:$SHA
+
